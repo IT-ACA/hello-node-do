f627fafd434324a19c5028d369a677b48d14da34
IT-ACA <65686992+IT-ACA@users.noreply.github.com>
2021-08-03 12:47:47 -0400

    Update index.js
    Change testing CI/CD for DO droplet after adding DO SSH key to Github

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node for Digital Ocean on Deploy!\n'
+  const msg = 'Hello Node for Digital Ocean CI/CD Deploy!\n'
   res.end(msg);
 });
 
