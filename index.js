2016219531160ef84c4eeefa3dff86e7224102f7
IT-ACA <65686992+IT-ACA@users.noreply.github.com>
2021-08-02 22:48:14 -0400

    Update index.js
    Test commit for Digital Ocean deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node for Digital Ocean!\n'
+  const msg = 'Hello Node for Digital Ocean on Deploy!\n'
   res.end(msg);
 });
 
